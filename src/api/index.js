0ff8df752a8d65507e0ba281c07b89ab1a78bc51
gitty-girl <mariam.tchanturia1994@gmail.com>
2021-06-23 21:31:29 +0400

    add try catch block in api calls

--- src/api/index.js
+++ src/api/index.js
@@ -4,63 +4,83 @@ const GITHUB_API_URL = process.env.REACT_APP_GITHUB_API_URL;
 const GITHUB_API_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 async function getUsers({ page = 1 }) {
-  const response = await axios.request({
-    method: "GET",
-    url: `${GITHUB_API_URL}/search/users?q=followers:%3E=1000&per_page=20&page=${page}`,
-    headers: {
-      Authorization: `token ${GITHUB_API_TOKEN}`,
-    },
-  });
+  try {
+    const response = await axios.request({
+      method: "GET",
+      url: `${GITHUB_API_URL}/search/users?q=followers:%3E=1000&per_page=20&page=${page}`,
+      headers: {
+        Authorization: `token ${GITHUB_API_TOKEN}`,
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function getUserDetails({ login }) {
-  const response = await axios.request({
-    method: "GET",
-    url: `${GITHUB_API_URL}/users/${login}`,
-    headers: {
-      Authorization: `token ${GITHUB_API_TOKEN}`,
-    },
-  });
+  try {
+    const response = await axios.request({
+      method: "GET",
+      url: `${GITHUB_API_URL}/users/${login}`,
+      headers: {
+        Authorization: `token ${GITHUB_API_TOKEN}`,
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function getUserOrganisations({ login }) {
-  const response = await axios.request({
-    method: "GET",
-    url: `${GITHUB_API_URL}/users/${login}/orgs`,
-    headers: {
-      Authorization: `token ${GITHUB_API_TOKEN}`,
-    },
-  });
+  try {
+    const response = await axios.request({
+      method: "GET",
+      url: `${GITHUB_API_URL}/users/${login}/orgs`,
+      headers: {
+        Authorization: `token ${GITHUB_API_TOKEN}`,
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function getUserRepositories({ login }) {
-  const response = await axios.request({
-    method: "GET",
-    url: `${GITHUB_API_URL}/users/${login}/repos`,
-    headers: {
-      Authorization: `token ${GITHUB_API_TOKEN}`,
-    },
-  });
+  try {
+    const response = await axios.request({
+      method: "GET",
+      url: `${GITHUB_API_URL}/users/${login}/repos`,
+      headers: {
+        Authorization: `token ${GITHUB_API_TOKEN}`,
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 async function findUsersByUsername({ username }) {
-  const response = await axios.request({
-    method: "GET",
-    url: `${GITHUB_API_URL}/search/users?q=${username}`,
-    headers: {
-      Authorization: `token ${GITHUB_API_TOKEN}`,
-    },
-  });
+  try {
+    const response = await axios.request({
+      method: "GET",
+      url: `${GITHUB_API_URL}/search/users?q=${username}`,
+      headers: {
+        Authorization: `token ${GITHUB_API_TOKEN}`,
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
 }
 
 export {
